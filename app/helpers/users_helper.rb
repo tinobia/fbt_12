da5ee2c550139a2f4e8acc9b2c590cd21e7b20b9
Tan Huynh <huynh.tan@framgia.com>
2017-11-10 13:23:00 +0700

    Migrate old front end to use the new template

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
   def full_name user
-    if user.first_name && user.last_name
+    if user.first_name.present? || user.last_name.present?
       "#{user.first_name} #{user.last_name}"
     else
       t "shared.anonymous"
