f3047cc472ef9362dada8c1d9753a5e4d2c11a31
Tan Huynh <huynh.tan@framgia.com>
2017-11-20 14:08:08 +0700

    remove old obsolete tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,26 +7,5 @@ module ActiveSupport
     include AbstractController::Translation
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
-
-    # Returns true if a test user is logged in.
-    def is_logged_in?
-      session[:user_id].present?
-    end
-
-    # Log in as a particular user.
-    def log_in_as user
-      session[:user_id] = user.id
-    end
-  end
-end
-
-module ActionDispatch
-  class IntegrationTest
-    # Log in as a particular user.
-    def log_in_as user, password: "password", remember_me: Settings.remember_me
-      post login_path, params: {session: {username: user.username,
-                                          password: password,
-                                          remember_me: remember_me}}
-    end
   end
 end
